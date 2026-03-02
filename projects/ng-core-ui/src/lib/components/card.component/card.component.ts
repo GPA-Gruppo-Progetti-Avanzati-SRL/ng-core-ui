e9f2871fc1114f37aad3d54f2be17e8373278f14
Karina <karinakate0@gmail.com>
2026-03-02 16:06:56 +0100

    init card component

---
+++ projects/ng-core-ui/src/lib/components/card.component/card.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'lib-card',
+  imports: [CommonModule, MatIconModule, MatButtonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css',
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() subtitle?: string = '';
+  @Input() icon: string = 'info';
+  @Input() buttonLabel: string = 'Procedi';
+  @Input() buttonAction: () => void = () => {};
+}
