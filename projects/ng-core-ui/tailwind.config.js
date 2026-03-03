4a0b95b5b7491f8399bc7c6ca7c147d9b2f0cc71
simone.tiberti <simone.tiberti@gpagroup.it>
2026-03-03 19:24:56 +0100

    Integrate Tailwind CSS into `ng-core-ui`
    - Add PostCSS and TailwindCSS dependencies to `package.json`.
    - Configure Tailwind with a new `tailwind.config.js` under `projects/ng-core-ui`.
    - Update `themes.scss` to include Tailwind's base styles using `@use "tailwindcss"`.
    - Adjust component styles (e.g., `card.component.html`) to utilize Tailwind utility classes.
    - Add `.postcssrc.json` file for PostCSS configuration.

---
+++ projects/ng-core-ui/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+
+
+export default {
+  content: [
+    './src/**/*.ts'
+  ],
+  darkMode: 'class',
+  important: '.ui',
+  plugins: [],
+  prefix: 'ui:',
+}
