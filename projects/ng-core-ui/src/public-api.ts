cf690385c6597af6648cd1c6acff9039b712d5b9
simone.tiberti <simone.tiberti@gpagroup.it>
2026-04-09 10:33:21 +0200

    Organizzazione `public-api.ts`.

--- projects/ng-core-ui/src/public-api.ts
+++ projects/ng-core-ui/src/public-api.ts
@@ -1,16 +1,23 @@
 
+//SYSTEM
 export * from './lib/system/system.models';
 export * from './lib/system/environment';
 export * from './lib/system/system.service';
 export * from './lib/system/style-manager.service';
 export * from './lib/system/menu.guard';
 export * from './lib/system/routes';
+
+//DEFAULT PAGES
 export * from './lib/not-found/not-found.component';
 export * from './lib/forbidden/forbidden.component';
+//LAYOUT
 export * from './lib/layout/main-layout/main-layout.component';
 export * from './lib/layout/simple-layout/simple-layout.component';
-export * from './lib/main'
+
 
 // COMPONENTI
 export * from './lib/components/card.component/card.component';
 export * from './lib/components/topbar.component/topbar.component';
+
+
+export * from './lib/main'
