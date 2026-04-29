4897a1f63212b15f9c19298d652fe60fe6a5d160
simone.tiberti <simone.tiberti@gpagroup.it>
2026-04-29 12:02:01 +0200

    Update `datatable` template and enhance `update-package-description` schematic
    - Remove "Actions" header cell from `datatable.component.html`.
    - Add conditional `type: module` assignment to `package.json` in schematic.

--- projects/ng-core-ui/schematics/ng-add/steps/update-package-description.ts
+++ projects/ng-core-ui/schematics/ng-add/steps/update-package-description.ts
@@ -3,12 +3,19 @@ import { Schema } from '../types';
 
 export function updatePackageDescription(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    if (!options.description) return;
-
     const pkgPath = 'package.json';
     const pkg = JSON.parse(tree.read(pkgPath)!.toString('utf-8'));
-    pkg.description = options.description;
+
+    if (options.description) {
+      pkg.description = options.description;
+      context.logger.info(`  ✔ description aggiunta in package.json`);
+    }
+
+    if (pkg.type !== 'module') {
+      pkg.type = 'module';
+      context.logger.info(`  ✔ "type": "module" aggiunto in package.json`);
+    }
+
     tree.overwrite(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
-    context.logger.info(`  ✔ description aggiunta in package.json`);
   };
 }
