2d98c31d0563f25c5e8809d0d1d787a5298b8312
Karina <karinakate0@gmail.com>
2026-04-29 17:29:05 +0200

    fields, checkbox, radiobutton style

--- projects/ng-core-ui/src/lib/components/form-shell.component/fields/combobox-field.component.ts
+++ projects/ng-core-ui/src/lib/components/form-shell.component/fields/combobox-field.component.ts
@@ -1,24 +1,32 @@
-import { ChangeDetectionStrategy, Component, Signal, ViewEncapsulation, computed, input, isSignal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Signal,
+  ViewEncapsulation,
+  computed,
+  input,
+  isSignal,
+} from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 import { FormField } from '@angular/forms/signals';
 import { KVOption } from '../form-field.models';
 
-
 @Component({
   selector: 'core-combobox-field',
-  imports: [MatFormFieldModule, MatSelectModule, FormField],
+  imports: [MatFormFieldModule, MatSelectModule, MatOptionModule, FormField],
   templateUrl: './combobox-field.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ComboboxFieldComponent {
-  readonly formField   = input.required<any>();
-  readonly options     = input<KVOption[] | Signal<KVOption[]>>([]);
-  readonly label       = input<string>('');
+  readonly formField = input.required<any>();
+  readonly options = input<KVOption[] | Signal<KVOption[]>>([]);
+  readonly label = input<string>('');
 
   readonly resolvedOptions = computed<KVOption[]>(() => {
     const opts = this.options();
-    return isSignal(opts) ? (opts as Signal<KVOption[]>)() : opts as KVOption[];
+    return isSignal(opts) ? (opts as Signal<KVOption[]>)() : (opts as KVOption[]);
   });
 }
